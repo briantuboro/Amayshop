1355e8c09af61cbb4e41eee9f5025ef59aa9d955
briantuboro <briantuboro@192.168.0.13>
2017-05-10 08:17:23 +0800

    Code changes for adding item to shopping cart

---
+++ Amayshop/src/com/shoppingcartexercise/program/ShoppingCart.java
@@ -0,0 +1,5 @@
+package com.shoppingcartexercise.program;
+
+public class ShoppingBasketCart {
+
+}
---
+++ Amayshop/test/com/shoppingcartexercise/program/ShoppingCartTest.java
@@ -0,0 +1,20 @@
+package com.shoppingcartexercise.program;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class ShoppingCartExerciseTest {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@Test
+	public void test() {
+		
+		ShoppingBasketCart shoppingBasketCart = new ShoppingBasketCart();
+	}
+
+}
